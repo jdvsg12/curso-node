1011e18e57d3760d74b1b901ef76de89363b174f
Julian Velandia <julianvelandia@flare.com.co>
2024-07-27 16:19:12 -0500

    add:class 1 node js

---
+++ cjs/index.js
@@ -0,0 +1,4 @@
+const { suma, resta } = require('./calculate')
+
+console.log(suma(3, 4))
+console.log(resta(4, 7))
---
+++ mjs/index.mjs
@@ -0,0 +1,4 @@
+import { suma, resta } from './calculator.mjs'
+
+console.log(suma(3, 4))
+console.log(resta(4, 7))
